c1024298bd2a8cca287452a18960e97ba789c02e
Jacob Lifshay <programmerjake@gmail.com>
2021-06-24 21:36:46 -0700

    add assign_data_in to test_reg

--- tests/test_reg.rs
+++ tests/test_reg.rs
@@ -12,7 +12,7 @@ macro_rules! assert_formats_to {
 
 #[test]
 fn test_reg() {
-    Context::with(|ctx| {
+    Context::with(|ctx: ContextRef| {
         named!(let (top, cd) = ctx.top_module(ClockDomain::default().get_input(ctx)));
         named!(let reg = top.reg(cd.get(), UInt32::default()));
         assert_formats_to!(
@@ -236,6 +236,173 @@ Reg {
             value: 0x0,
         },
         ..
+    },
+    ..
+}"#
+        );
+        let reg_output = reg.output();
+        reg.assign_data_in(UInt32::wrapping_new(0x1234).get_value(ctx));
+        assert_formats_to!(
+            reg_output,
+            r#"
+Val {
+    ir: IrRegOutput {
+        path: "top"."reg",
+        value_type: BitVector {
+            bit_count: 32,
+        },
+        ..
+    },
+    value_type: ValueType(
+        BitVector {
+            bit_count: 32,
+        },
+    ),
+}"#
+        );
+        assert_formats_to!(
+            top,
+            r#"
+IrModule {
+    path: "top",
+    parent: <None>,
+    interface_types: [
+        Input(
+            IrStructType {
+                fields: [
+                    IrStructFieldType {
+                        name: "clk",
+                        ty: BitVector {
+                            bit_count: 1,
+                        },
+                    },
+                    IrStructFieldType {
+                        name: "rst",
+                        ty: BitVector {
+                            bit_count: 1,
+                        },
+                    },
+                ],
+            },
+        ),
+    ],
+    interface_write_ends: [
+        Input(
+            LiteralStruct {
+                ty: IrStructType {
+                    fields: [
+                        IrStructFieldType {
+                            name: "clk",
+                            ty: BitVector {
+                                bit_count: 1,
+                            },
+                        },
+                        IrStructFieldType {
+                            name: "rst",
+                            ty: BitVector {
+                                bit_count: 1,
+                            },
+                        },
+                    ],
+                },
+                owning_module: None,
+                fields: [
+                    LiteralStructField {
+                        name: "clk",
+                        value: LiteralBits {
+                            bit_count: 1,
+                            value: 0x0,
+                        },
+                    },
+                    LiteralStructField {
+                        name: "rst",
+                        value: LiteralBits {
+                            bit_count: 1,
+                            value: 0x0,
+                        },
+                    },
+                ],
+            },
+        ),
+    ],
+    wires: {},
+    registers: {
+        "reg": IrReg {
+            value_type: BitVector {
+                bit_count: 32,
+            },
+            data_in: LiteralBits {
+                bit_count: 32,
+                value: 0x1234,
+            },
+            clk: ExtractStructField {
+                struct_value: IrModuleInput {
+                    module: "top",
+                    index: 0,
+                    path: "io",
+                    value_type: IrStructType {
+                        fields: [
+                            IrStructFieldType {
+                                name: "clk",
+                                ty: BitVector {
+                                    bit_count: 1,
+                                },
+                            },
+                            IrStructFieldType {
+                                name: "rst",
+                                ty: BitVector {
+                                    bit_count: 1,
+                                },
+                            },
+                        ],
+                    },
+                },
+                struct_field_type: IrStructFieldType {
+                    name: "clk",
+                    ty: BitVector {
+                        bit_count: 1,
+                    },
+                },
+                field_index: 0,
+                ..
+            },
+            reset_enable: ExtractStructField {
+                struct_value: IrModuleInput {
+                    module: "top",
+                    index: 0,
+                    path: "io",
+                    value_type: IrStructType {
+                        fields: [
+                            IrStructFieldType {
+                                name: "clk",
+                                ty: BitVector {
+                                    bit_count: 1,
+                                },
+                            },
+                            IrStructFieldType {
+                                name: "rst",
+                                ty: BitVector {
+                                    bit_count: 1,
+                                },
+                            },
+                        ],
+                    },
+                },
+                struct_field_type: IrStructFieldType {
+                    name: "rst",
+                    ty: BitVector {
+                        bit_count: 1,
+                    },
+                },
+                field_index: 1,
+                ..
+            },
+            reset_value: LiteralBits {
+                bit_count: 32,
+                value: 0x0,
+            },
+            ..
+        },
     },
     ..
 }"#
