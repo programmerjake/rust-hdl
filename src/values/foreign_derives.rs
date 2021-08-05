789323724d5a45e061b19965e2ce7a4a51446283
Jacob Lifshay <programmerjake@gmail.com>
2021-08-04 18:34:56 -0700

    tuple fields should be public

--- src/values/foreign_derives.rs
+++ src/values/foreign_derives.rs
@@ -22,55 +22,82 @@ pub type Tuple1<T1> = (T1,);
 #[derive(Value, FixedTypeValue)]
 #[rust_hdl(crate = crate, real_type_name = Tuple1)]
 #[allow(dead_code)]
-pub struct MyTuple1<T1>(T1);
+pub struct MyTuple1<T1>(pub T1);
 
 pub type Tuple2<T1, T2> = (T1, T2);
 #[derive(Value, FixedTypeValue)]
 #[rust_hdl(crate = crate, real_type_name = Tuple2)]
 #[allow(dead_code)]
-pub struct MyTuple2<T1, T2>(T1, T2);
+pub struct MyTuple2<T1, T2>(pub T1, pub T2);
 
 pub type Tuple3<T1, T2, T3> = (T1, T2, T3);
 #[derive(Value, FixedTypeValue)]
 #[rust_hdl(crate = crate, real_type_name = Tuple3)]
 #[allow(dead_code)]
-pub struct MyTuple3<T1, T2, T3>(T1, T2, T3);
+pub struct MyTuple3<T1, T2, T3>(pub T1, pub T2, pub T3);
 
 pub type Tuple4<T1, T2, T3, T4> = (T1, T2, T3, T4);
 #[derive(Value, FixedTypeValue)]
 #[rust_hdl(crate = crate, real_type_name = Tuple4)]
 #[allow(dead_code)]
-pub struct MyTuple4<T1, T2, T3, T4>(T1, T2, T3, T4);
+pub struct MyTuple4<T1, T2, T3, T4>(pub T1, pub T2, pub T3, pub T4);
 
 pub type Tuple5<T1, T2, T3, T4, T5> = (T1, T2, T3, T4, T5);
 #[derive(Value, FixedTypeValue)]
 #[rust_hdl(crate = crate, real_type_name = Tuple5)]
 #[allow(dead_code)]
-pub struct MyTuple5<T1, T2, T3, T4, T5>(T1, T2, T3, T4, T5);
+pub struct MyTuple5<T1, T2, T3, T4, T5>(pub T1, pub T2, pub T3, pub T4, pub T5);
 
 pub type Tuple6<T1, T2, T3, T4, T5, T6> = (T1, T2, T3, T4, T5, T6);
 #[derive(Value, FixedTypeValue)]
 #[rust_hdl(crate = crate, real_type_name = Tuple6)]
 #[allow(dead_code)]
-pub struct MyTuple6<T1, T2, T3, T4, T5, T6>(T1, T2, T3, T4, T5, T6);
+pub struct MyTuple6<T1, T2, T3, T4, T5, T6>(pub T1, pub T2, pub T3, pub T4, pub T5, pub T6);
 
 pub type Tuple7<T1, T2, T3, T4, T5, T6, T7> = (T1, T2, T3, T4, T5, T6, T7);
 #[derive(Value, FixedTypeValue)]
 #[rust_hdl(crate = crate, real_type_name = Tuple7)]
 #[allow(dead_code)]
-pub struct MyTuple7<T1, T2, T3, T4, T5, T6, T7>(T1, T2, T3, T4, T5, T6, T7);
+pub struct MyTuple7<T1, T2, T3, T4, T5, T6, T7>(
+    pub T1,
+    pub T2,
+    pub T3,
+    pub T4,
+    pub T5,
+    pub T6,
+    pub T7,
+);
 
 pub type Tuple8<T1, T2, T3, T4, T5, T6, T7, T8> = (T1, T2, T3, T4, T5, T6, T7, T8);
 #[derive(Value, FixedTypeValue)]
 #[rust_hdl(crate = crate, real_type_name = Tuple8)]
 #[allow(dead_code)]
-pub struct MyTuple8<T1, T2, T3, T4, T5, T6, T7, T8>(T1, T2, T3, T4, T5, T6, T7, T8);
+pub struct MyTuple8<T1, T2, T3, T4, T5, T6, T7, T8>(
+    pub T1,
+    pub T2,
+    pub T3,
+    pub T4,
+    pub T5,
+    pub T6,
+    pub T7,
+    pub T8,
+);
 
 pub type Tuple9<T1, T2, T3, T4, T5, T6, T7, T8, T9> = (T1, T2, T3, T4, T5, T6, T7, T8, T9);
 #[derive(Value, FixedTypeValue)]
 #[rust_hdl(crate = crate, real_type_name = Tuple9)]
 #[allow(dead_code)]
-pub struct MyTuple9<T1, T2, T3, T4, T5, T6, T7, T8, T9>(T1, T2, T3, T4, T5, T6, T7, T8, T9);
+pub struct MyTuple9<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+    pub T1,
+    pub T2,
+    pub T3,
+    pub T4,
+    pub T5,
+    pub T6,
+    pub T7,
+    pub T8,
+    pub T9,
+);
 
 pub type Tuple10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> =
     (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
@@ -78,16 +105,16 @@ pub type Tuple10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> =
 #[rust_hdl(crate = crate, real_type_name = Tuple10)]
 #[allow(dead_code)]
 pub struct MyTuple10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
-    T1,
-    T2,
-    T3,
-    T4,
-    T5,
-    T6,
-    T7,
-    T8,
-    T9,
-    T10,
+    pub T1,
+    pub T2,
+    pub T3,
+    pub T4,
+    pub T5,
+    pub T6,
+    pub T7,
+    pub T8,
+    pub T9,
+    pub T10,
 );
 
 pub type Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> =
@@ -96,17 +123,17 @@ pub type Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> =
 #[rust_hdl(crate = crate, real_type_name = Tuple11)]
 #[allow(dead_code)]
 pub struct MyTuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(
-    T1,
-    T2,
-    T3,
-    T4,
-    T5,
-    T6,
-    T7,
-    T8,
-    T9,
-    T10,
-    T11,
+    pub T1,
+    pub T2,
+    pub T3,
+    pub T4,
+    pub T5,
+    pub T6,
+    pub T7,
+    pub T8,
+    pub T9,
+    pub T10,
+    pub T11,
 );
 
 pub type Tuple12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> =
@@ -115,16 +142,16 @@ pub type Tuple12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> =
 #[rust_hdl(crate = crate, real_type_name = Tuple12)]
 #[allow(dead_code)]
 pub struct MyTuple12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(
-    T1,
-    T2,
-    T3,
-    T4,
-    T5,
-    T6,
-    T7,
-    T8,
-    T9,
-    T10,
-    T11,
-    T12,
+    pub T1,
+    pub T2,
+    pub T3,
+    pub T4,
+    pub T5,
+    pub T6,
+    pub T7,
+    pub T8,
+    pub T9,
+    pub T10,
+    pub T11,
+    pub T12,
 );
